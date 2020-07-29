215e1a1cf1015513ba9901159e85a515cc9e8654
Sandro Santos <sandro.dev@gmail.com>
2020-07-29 16:34:59 -0300

    SignIn page with components Input and Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
 
 import { Container, Content, Background} from './style';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -17,11 +20,11 @@ const SignIn: React.FC = () => (
 
         <h1>Faça seu login</h1>
 
-        <input type="text" placeholder="E-mail"/>
+        <Input name="email" icon={FiMail} type="text" placeholder="E-mail"/>
 
-        <input type="password" placeholder="Senha"/>
+        <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
 
--- src/pages/SignIn/style.ts
+++ src/pages/SignIn/style.ts
@@ -29,42 +29,6 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    input {
-      width: 100%;
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      color: #f4ede8;
-
-      &::placeholder {
-        color: #666360;
-      }
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button {
-      width: 100%;
-      height: 56px;
-      margin-top: 16px;
-
-      background: #FF9000;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      color: #312E38;
-      font-weight: 500;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background: ${shade(0.2, '#ff9000')};
-      }
-
-    }
-
     a {
       color: #f4ede8;
       display: block;
